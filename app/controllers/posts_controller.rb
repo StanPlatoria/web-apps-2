975d8b1c6483c727f882e8465fa5ace3490587aa
Stan <120746167+StanPlatoria@users.noreply.github.com>
2026-02-17 17:28:07 +0000

    ....

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,24 @@
 class PostsController < ApplicationController
-
   def index
-    # find all Post rows
     @posts = Post.all
-    # render posts/index view
   end
 
   def new
-    # render posts/new view with new Post form
+    @post = Post.new
   end
 
-  # def create
-  #   # start with a new Post
-  #   # assign user-entered form data to Post's columns
-  #   # save Post row
-  #   # redirect user
-  # end
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
 
+  private
+
+  def post_params
+    params.permit(:author, :body, :image)
+  end
 end
