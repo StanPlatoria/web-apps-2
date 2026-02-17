a68d100d953a24acccdfbb7c42fc8e353124f2e9
Stan <120746167+StanPlatoria@users.noreply.github.com>
2026-02-17 15:54:30 +0000

    .

--- app/controllers/bitcoins_controller.rb
+++ app/controllers/bitcoins_controller.rb
@@ -2,6 +2,22 @@ class BitcoinsController < ApplicationController
 
   def index
     # add backend code
+  @bitcoin = 1.0
+
+  require "net/http"
+  require "json"
+  url = "https://blockchain.info/ticker"
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  bitcoin_data = JSON.parse(response)
+  @bitcoin = params["amount"].to_f
+
+  # using the API, extract current rate of bitcoin in USD
+  @usd_rate = bitcoin_data["USD"]["last"]
+
+  # calculate value in USD of user's bitcoin
+  @usd_value = @usd_rate * @bitcoin
+
     # render bitcoins/index view
   end
 
